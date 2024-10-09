7289b683789270d0e200a9a45769633884733326
Aymen <adouniaymen91@gmail.com>
2024-10-09 12:41:04 +0100

    missing title the moneytest.js file

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,15 +1,24 @@
 import currencyFormat from "../scripts/utils/money.js";
 
+console.log('Test suite: formatCurrency');
+console.log('converts cents to dollars');
+
 if (currencyFormat(2095) === '20.95') {
   console.log('passed');
 } else {
   console.log('failed');
 }
+
+console.log('works with 0');
+
 if (currencyFormat(0) === '0.00') {
   console.log('passed');
 } else {
   console.log('failed');
 }
+
+console.log('round up to the nearest cent');
+
 if (currencyFormat(2000.5) === '20.01') {
   console.log('passed');
 } else {
